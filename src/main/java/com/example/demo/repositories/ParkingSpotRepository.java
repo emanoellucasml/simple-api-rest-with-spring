666f7af8b8f55db618b520c014cde08e191bc65e
Emanoel Lucas <emanoel.moraes@mercadolivre.com>
2023-01-23 23:38:02 -0300

    first commit

---
+++ src/main/java/com/example/demo/repositories/ParkingSpotRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.ParkinSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkinSpotModel, UUID> {
+
+}

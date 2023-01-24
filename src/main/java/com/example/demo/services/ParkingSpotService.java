cb62ac51f7869bbf0566b9f1a66464d3ba8a51c0
Emanoel Lucas <emanoel.moraes@mercadolivre.com>
2023-01-23 23:44:05 -0300

    create ParkingSpotController

--- src/main/java/com/example/demo/services/ParkingSpotService.java
+++ src/main/java/com/example/demo/services/ParkingSpotService.java
@@ -1,8 +1,15 @@
 package com.example.demo.services;
 
+import com.example.demo.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingSpotService {
 
+    final ParkingSpotRepository parkingSpotRepository;
+
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
 }
